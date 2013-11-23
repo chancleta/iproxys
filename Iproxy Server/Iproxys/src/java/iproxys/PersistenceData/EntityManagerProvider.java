b80aab74008a6f1524a92a3c12fe2bea7a0059ad
root <root@luis-pc.(none)>
2013-11-23 17:54:48 -0400

    Fixing double execution

---
+++ Iproxy Server/Iproxys/src/java/iproxys/PersistenceData/EntityManagerProvider.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iproxys.PersistenceData;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author root
+ */
+public class EntityManagerProvider {
+
+    private static EntityManagerProvider entityManagerProvider = null;
+    public static final String PERSISTENCE_UNIT = "TestJEE-ejbPU";
+    private EntityManagerFactory entityFactory;
+    private EntityManager entityManager;
+
+    private EntityManagerProvider() {
+        entityFactory = Persistence.createEntityManagerFactory(EntityManagerProvider.PERSISTENCE_UNIT);
+        entityManager = entityFactory.createEntityManager();
+    }
+
+    public static EntityManagerProvider getInstance() {
+        if (entityManagerProvider == null) {
+            entityManagerProvider = new EntityManagerProvider();
+        }
+        return entityManagerProvider;
+    }
+    public EntityManager getEntityManager(){
+        return entityManager;
+    }
+}
